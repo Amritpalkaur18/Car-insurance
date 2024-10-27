7989501a665ce8973f8f5737d4a9cd06e88c21da
Amritpalkaur18 <palamrit0218@gmail.com>
2024-10-27 16:36:53 -0400

    Add project files.

---
+++ Car insurance/Program.cs
@@ -0,0 +1,15 @@
+﻿< h2 > Recent Projects </ h2 >
+   < ul >
+   
+       < li >< strong > Car Insurance Calculator</strong>
+         <a href = "C:\Users\HP\source\repos\Car insurance\Car insurance" > View on GitHub</a></li>  
+    <li><strong>NFL Stats Tracker</strong>: 
+       
+</ul>
+
+<h2>Technical Skills</h2>
+<ul>
+    <li>C# Programming</li>
+    <li>.NET Framework</li>
+    <li>Object-Oriented Programming (OOP)</li>
+</ul>
